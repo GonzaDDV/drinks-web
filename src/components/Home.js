eb529e8dd3034b096f93b594399f5c0a7476ef47
GonzaDDV <gonzalodiazdevivar@gmail.com>
2020-07-26 21:08:51 -0300

    :white_check_mark: Verificación de mayoría de edad

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,8 +1,25 @@
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import axios from "axios";
+import Cookies from "universal-cookie";
 
 const Home = () => {
+  const cookies = new Cookies();
+  const [accepted, setAccepted] = useState(false);
+
+  useEffect(() => {
+    if (cookies.get("accepted")) {
+      setAccepted(true);
+    }
+  }, []);
+
+  const handleAcceptCookies = () => {
+    const timeToExpire = new Date(2030, 12);
+
+    cookies.set("accepted", true, { expires: timeToExpire });
+    setAccepted(true);
+  };
+
   const [rndDrink, setRndDrink] = useState({});
   const [loading, setLoading] = useState(true);
 
@@ -19,6 +36,14 @@ const Home = () => {
 
   return (
     <main className="home">
+      {!accepted && (
+        <div className="container-mayor-edad">
+          <h1>Para entrar a la plataforma, tienes que ser mayor de 18 años.</h1>
+          <button onClick={() => handleAcceptCookies()}>
+            Soy mayor de 18 años!
+          </button>
+        </div>
+      )}
       <h1>Trago aleatorio</h1>
       <div className="trago-home">
         <div id="skeleton" className={!loading ? "finished" : null}></div>
